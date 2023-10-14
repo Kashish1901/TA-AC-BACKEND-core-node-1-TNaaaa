ace9224fad453d7b01824da43df5356d695618e1
KASHISH <kashu80525@gmail.com>
2023-10-14 21:28:59 +0530

    adding codes

--- block-BNaaap/index.js
+++ block-BNaaap/index.js
@@ -1,8 +1,9 @@
 var http = require("http");
 var server = http.createServer(handleRequest);
 function handleRequest(req, res) {
+  console.log(req.methods);
   res.statusCode = 201;
-  res.setHeader("Content-Type", text / html);
+  res.setHeader("content-Type", "text / html");
   res.end("<h1>Welcome</h1>");
 }
 
